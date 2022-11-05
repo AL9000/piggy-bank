2c1a6f778dd3ecbbd08b2f83eeb77c9a3f497a4e
jmr <julien.marzin@makina-corpus.com>
2022-11-05 22:22:29 +0100

    add django-solo for singleton management

--- piggybank/settings.py
+++ piggybank/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # project's apps
     'savings.apps.SavingsConfig',
+    # third parties
+    'solo.apps.SoloAppConfig',
 ]
 
 MIDDLEWARE = [

a10f112adc1a5f4c8e9e20c8821ebf50c9b5a787
jmr <julien.marzin@makina-corpus.com>
2022-11-05 23:06:00 +0100

    tdd: shake piggy bank must returns its savings amount or an error

--- savings/tests.py
+++ savings/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.shortcuts import resolve_url
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from savings.models import PiggyBank
+
+
+class PyggyBankShakingTestCase(APITestCase):
+    def test_shake_piggybank_must_returns_an_error_if_the_piggybank_was_not_created_before(self):
+        url = resolve_url("shake-piggybank")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 412)
+        self.assertEqual(
+            response.data,
+            "You don't have any savings yet! Make some before you shake your "
+            "piggy bank again."
+        )
+
+    def test_shake_piggybank_must_returns_its_savings_amount(self):
+        PiggyBank.objects.create(savings=2000)  # 2000 euro cents = 20€
+        url = resolve_url("shake-piggybank")
+        response = self.client.get(url)
+        self.assertEqual(response.data, 2000)
